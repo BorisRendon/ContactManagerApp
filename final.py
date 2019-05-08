d2d8b238fc56d0a8565a24c10b11ee00a6d03e40
fabricio63 <fabriciojuarez@ufm.edu>
2019-05-08 16:54:29 -0600

    fase 1 pendiente revision

--- final.py
+++ final.py
@@ -1,24 +1,37 @@
-contactos={1:{"nombre":"fabricio","apellido":"juarez","numero":"44"}}
+contactos=[{"nombre":"fabricio","apellido":"perez","numero":"44"},{"nombre":"andres","apellido":"alvarado","numero":"46"}]
 
-
-
-def agregar_contacto(codigoUnico,nombreNuevo,apellidoNuevo,numeroNuevo):
-    contactos[codigoUnico]= {"nombre":nombreNuevo,"apellido":apellidoNuevo,"numero":numeroNuevo}
+def agregar_contacto(nombreNuevo,apellidoNuevo,numeroNuevo):
+    new_contact= {"nombre":nombreNuevo,"apellido":apellidoNuevo,"numero":numeroNuevo}
+    contactos.append(new_contact)
+    print (contactos)
 
 def listar_contactos(contactos):
+    print("---------------------------")
+    print("---------------------------")
     for k in (contactos):
-        print (k)
-        for n,v in dict.keys(k):
-            print (n)   
-# agregar_contacto("2","fabricio","perez","65")
+        contador=0
+        for i,n in k.items():
+            contador = contador +1
+            print("-{} es:  {}".format(i,n))
+        print("---------------------------")
+        print("---------------------------")
+listar_contactos(contactos)
+            
 def eliminar_contactos (nombre):
-    nombre_lista=list(nombre)
+    nombre_lista= nombre.split(" ")
     nombre_cliente= nombre_lista[0]
-    apellido_cliente = nombre lista[1]
+    apellido_cliente = nombre_lista[1]
+    conta = 0
+    for i in contactos:
+        if i["nombre"]==nombre_cliente:
+            if i["apellido"] == apellido_cliente:
+                eliminar = contactos[conta]
+                contactos.remove(eliminar)
+        conta = conta +1
+    print(contactos)
+
         
-# print(contactos)
 
-# listar_contactos(contactos)
+        
 
-eliminar_contactos(fabricio perez)
-    
+        
